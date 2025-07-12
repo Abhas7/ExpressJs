1438c415b67793904afcb5d1b7b8b4bc7c4f579f
abhassomkuwar <abhassomkuwar7@gmail.com>
2025-07-12 18:51:07 +0530

    first commit

---
+++ miniproject/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to logger api');
+
+})
+
+app.listen(8000, () =>{
+    console.log(`Server is running on port ${PORT}`);
+    
+})
+
+
+
+
